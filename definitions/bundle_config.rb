3dd1370b8988389203121abad8346e77c9919e2f
Marcin Nowicki <pr0d1r2@gmail.com>
2015-12-13 20:00:51 +0100

    Use bundler in path

--- definitions/bundle_config.rb
+++ definitions/bundle_config.rb
@@ -1,5 +1,5 @@
 define :bundle_config do
-  execute "#{node['etc']['passwd'][node['current_user']]['dir']}/.rbenv/shims/bundle config #{params[:name]}" do
+  execute "bundle config #{params[:name]}" do
     user node['current_user']
   end
 end
