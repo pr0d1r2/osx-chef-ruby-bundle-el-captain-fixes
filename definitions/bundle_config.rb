05898aae2560cd82e43e000c06028044c0c048f7
Marcin Nowicki <pr0d1r2@gmail.com>
2015-12-13 14:11:24 +0100

    Use bundle from user's rbenv

--- definitions/bundle_config.rb
+++ definitions/bundle_config.rb
@@ -1,5 +1,8 @@
 define :bundle_config do
   execute "bundle config #{params[:name]}" do
     user node['current_user']
+    environment({
+      'PATH' => "#{node['etc']['passwd'][node['current_user']]['dir']}/.rbenv/shims"
+    })
   end
 end
