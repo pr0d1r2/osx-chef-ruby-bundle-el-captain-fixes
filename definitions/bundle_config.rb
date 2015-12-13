8b0e0143e5ad2d118e1b21dbd4ff19315b734690
Marcin Nowicki <pr0d1r2@gmail.com>
2015-12-13 14:13:50 +0100

    Call user's rbenv bundle command directly

--- definitions/bundle_config.rb
+++ definitions/bundle_config.rb
@@ -1,8 +1,5 @@
 define :bundle_config do
-  execute "bundle config #{params[:name]}" do
+  execute "#{node['etc']['passwd'][node['current_user']]['dir']}/.rbenv/shims/bundle config #{params[:name]}" do
     user node['current_user']
-    environment({
-      'PATH' => "#{node['etc']['passwd'][node['current_user']]['dir']}/.rbenv/shims"
-    })
   end
 end
