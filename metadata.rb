96f35fe927948cd7c7ad48e38b96bf9b432b0c3f
Marcin Nowicki <pr0d1r2@gmail.com>
2015-12-13 10:51:10 +0100

    Add initial recipes

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ruby-bundle-el-captain-fixes'
+maintainer       'Marcin Nowicki'
+maintainer_email 'marcin@prodix.pl'
+license          'MIT'
+description      'Chef recipes to fix bundle builds of gem native extensions in OSX El Captain'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
